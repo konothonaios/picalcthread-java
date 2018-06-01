a119eb292ba53585aa12265f852d40c11fe9897a
Konstantinos Othonaios <35193403+mrkoth@users.noreply.github.com>
2018-06-02 00:17:27 +0300

    Update PiMain.java

--- PiMain.java
+++ PiMain.java
@@ -1,56 +1,54 @@
-class PiMain
-{
-	public static void main(String[] args)
-	{
-		// Megethos provlimatos
+class PiMain {
+	public static void main(String[] args) {
+		// Problem Size
 		int size = 1000;
 		
 		// Pi
 		double pi = 0.0;
 		
-		// Arxikopoiisi step
+		// Step
 		double step = 1.0 / (double)size;
 
-		// Dhmiourgia pinaka
+		// Create table
 		double[] a = new double[size];
 		for(int i = 0; i < size; i++)
 			a[i] = 0; 
 
-		// Pairnoume ton arithmo twn pirinwn gia na kanoume tosa nimata 
+		// Cores
 		int cores = Runtime.getRuntime().availableProcessors();
 		int numThreads = cores;
 	       
-		// Enarxi xronou
-        long start = System.currentTimeMillis();
+		// Start Time
+        	long start = System.currentTimeMillis();
 
-		// Dimiorgia nimatwn
+		// Create threads
 		PiThrdCalc threads[] = new PiThrdCalc[numThreads];
 		
-		// Enarxi nimatwn   
+		// Start threads   
 		for(int i = 0; i < numThreads; i++) {
 			threads[i] = new PiThrdCalc(i, numThreads, a, size, step);
 			threads[i].start();
 		}
 
-		// Anamoni na teleiwsoun ola ta nimata            
+		// Wait for all threads to finish
 		for(int i = 0; i < numThreads; i++) {
 			try {
 				threads[i].join();
            	} catch (InterruptedException e) {}
 		}
 
-		// Telikos ipologismos pi
+		// Final calculation of Pi
 		for(int i = 0; i < size; i++)
 			pi += a[i] * step;
 
-		// Telikos xronos
-	    long endTime = System.currentTimeMillis();
+		// End time
+		long endTime = System.currentTimeMillis();
 	    
-	    // Emfanisi apotelesmatwn
-        System.out.printf("Steps: %d \n", size);
-        System.out.printf("Calculated pi = %22.20f\n" ,pi);
-        System.out.printf("Difference between calculated pi and Math.PI = %22.20f\n", Math.abs(pi - Math.PI));
-        System.out.printf("Time = %f seconds\n", (double) (endTime - start)/1000);
+	   	// Printing
+        	System.out.printf("Steps: %d \n", size);
+        	System.out.printf("Calculated pi = %22.20f\n" ,pi);
+        	System.out.printf("Difference between calculated pi and Math.PI = %22.20f\n", Math.abs(pi - Math.PI));
+        	System.out.printf("Time = %f seconds\n", (double) (endTime - start)/1000);
 
 	}
 }
